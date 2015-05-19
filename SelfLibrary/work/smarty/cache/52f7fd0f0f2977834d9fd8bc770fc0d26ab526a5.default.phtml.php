ddd10ceaccbeac2105e16b821345c09319f1c21d
kimffy24 <kimffy24@gmail.com>
2015-05-19 08:57:29 +0800

    update

---
+++ SelfLibrary/work/smarty/cache/52f7fd0f0f2977834d9fd8bc770fc0d26ab526a5.default.phtml.php
@@ -0,0 +1,22 @@
+<?php /*%%SmartyHeaderCode:17130555a00408af361-13056540%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '52f7fd0f0f2977834d9fd8bc770fc0d26ab526a5' => 
+    array (
+      0 => 'D:\\web\\tbs.Test\\Test\\view\\index\\default.phtml',
+      1 => 1431960012,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '17130555a00408af361-13056540',
+  'variables' => 
+  array (
+    'uVersion' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_555a0040c6c4c3_92844076',
+  'cache_lifetime' => 3600,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_555a0040c6c4c3_92844076')) {function content_555a0040c6c4c3_92844076($_smarty_tpl) {?>18禁<?php }} ?>
